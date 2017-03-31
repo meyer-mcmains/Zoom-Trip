b954c9b98e8ea3ae380c1e524ad6ed26f4edb9df
Meyer McMains <meyer.mcmains@gmail.com>
2017-03-31 12:05:56 -0400

    Added Initial Files

---
+++ images/ReadMe.txt
@@ -0,0 +1,6 @@
+This folder is where the images to be loaded are stored
+
+The following file formats are currenlty supported:
+---------------------------------------------------
+png
+
---
+++ js/zoom.js
@@ -0,0 +1,92 @@
+var imgNum = 2;
+var main = 0;
+var interval;
+
+function zoom() {
+
+	var image = ($('.image'));
+	var nextImage = ($('.nextImage'));
+
+	if (main === 0)
+	{
+		imageMain(image);
+		imageSmall(nextImage);
+	}
+	else if (main === 1)
+	{
+		imageMain(nextImage);
+		imageSmall(image);
+	}
+
+	/*
+	if ($(window).height() < image.height() + 300 && main === 0)
+	{
+		changeImage(image);
+		main = 1;
+	}
+	else if ($(window).height() < nextImage.height() + 300 && main === 1)
+	{
+		changeImage(nextImage);
+		main = 0;
+	}
+	*/
+
+	if ($('.container').width() < image.width() || $('.container').height() < image.height() && main === 0)
+	{
+		changeImage(image);
+		main = 1;
+	}
+	else if ($('.container').width() < nextImage.width() || $('.container').height() < nextImage.height() && main === 1)
+	{
+		changeImage(nextImage);
+		main = 0;
+	}
+}
+
+$(document).on('wheel', zoom);
+
+$(document).mousedown(function() {
+    interval = setInterval(performWhileMouseDown, 50);
+}).mouseup(function() {
+    clearInterval(interval);  
+});
+
+function performWhileMouseDown() {
+    zoom();
+}
+
+function imageMain(image) {
+
+	var width = image.css('width');
+	width = parseInt(width, 10);
+	width += 20;
+
+	var opacity = image.css('opacity');
+	opacity = parseFloat(opacity, 10);
+	opacity += 0.05;
+	
+	image.css('width', width);
+	image.css('z-index', 1);
+	image.css('opacity', opacity);
+}
+
+function imageSmall(image) {
+
+	var width = image.css('width');
+	width = parseInt(width, 10);
+	width += 2;
+
+	var opacity = image.css('opacity');
+	opacity = parseFloat(opacity, 10);
+	opacity += 0.05;
+
+	image.css('width', width);
+	image.css('z-index', -1);
+}
+
+function changeImage(image) {
+	imgNum += 1;
+		image.attr('src', 'images/' + imgNum + '.png');
+	image.css('width', 10);
+	image.css('opacity', 0.5);
+}
