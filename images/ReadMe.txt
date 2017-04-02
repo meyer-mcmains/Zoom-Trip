c819df5e065c57f7daa45e6ba01425487f53f850
Meyer McMains <meyer.mcmains@gmail.com>
2017-04-02 15:32:22 -0400

    Added functionality to read the title from inside the SVG
    Also refactored some of the javascript code

--- images/ReadMe.txt
+++ images/ReadMe.txt
@@ -2,5 +2,5 @@ This folder is where the images to be loaded are stored
 
 The following file formats are currenlty supported:
 ---------------------------------------------------
-png
+svg
 
--- js/zoom.js
+++ js/zoom.js
@@ -2,6 +2,20 @@ var imgNum = 2;
 var main = 0;
 var interval;
 
+readSVG(imgNum);
+
+$(document).on('wheel', zoom);
+
+$(document).mousedown(function() {
+    interval = setInterval(performWhileMouseDown, 50);
+}).mouseup(function() {
+    clearInterval(interval);  
+});
+
+function performWhileMouseDown() {
+    zoom();
+}
+
 function zoom() {
 
 	var image = ($('.image'));
@@ -28,18 +42,6 @@ function zoom() {
 		changeImage(nextImage);
 		main = 0;
 	}
-}
-
-$(document).on('wheel', zoom);
-
-$(document).mousedown(function() {
-    interval = setInterval(performWhileMouseDown, 50);
-}).mouseup(function() {
-    clearInterval(interval);  
-});
-
-function performWhileMouseDown() {
-    zoom();
 }
 
 function imageMain(image) {
@@ -73,7 +75,30 @@ function imageSmall(image) {
 
 function changeImage(image) {
 	imgNum += 1;
-		image.attr('src', 'images/' + imgNum + '.png');
+		image.attr('src', 'images/' + imgNum + '.svg');
 	image.css('width', 10);
 	image.css('opacity', 0.5);
+
+	readSVG(imgNum);
+}
+
+function readSVG(imgNum) {
+	imgNum --;
+	var xhttp;
+	xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	        getName(this);
+	    }
+	};
+	xhttp.open("GET", "images/" + imgNum + ".svg", true);
+	xhttp.send();
+}
+
+function getName(xml) { 
+    var xmlDoc = xml.responseXML;
+    var tag = xmlDoc.getElementsByTagName("title")[0];
+    var text = tag.childNodes[0];
+    console.log(text);
+    $('#name').html(text);
 }
